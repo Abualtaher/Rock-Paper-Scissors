d907ee6c57977ac234aee03ea69d74ea76233991
Abualtaher <wisamaltaher@gmail.com>
2025-02-24 21:49:38 +0100

    added if statment and switch for the results

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.querySelector("#playerDisplay");
-const computerDisplay = document.querySelector("#computerDiplay");
+const computerDisplay = document.querySelector("#computerDisplay");
 const resultDiplay = document.querySelector("#resultDisplay");
 
 function playGame(playerChoice) {
   const computerChoice = choices[Math.floor(Math.random() * 3)];
-  console.log(computerChoice);
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "It is a tie!";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "You win!" : "You lose!";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "You win!" : "You lose!";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "You win!" : "You lose!";
+        break;
+    }
+  }
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  resultDiplay.textContent = result;
 }
